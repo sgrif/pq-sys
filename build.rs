af7d3a9b87298289422eb2e2919948902bdc0e80
Pyry Kontio <pyry.kontio@drasa.eu>
2016-11-23 14:20:41 +0200

    Added support for pkg-config

--- build.rs
+++ build.rs
@@ -1,11 +1,20 @@
+extern crate pkg_config;
+
 use std::fs;
 use std::path::PathBuf;
 use std::process::Command;
 use std::env;
 
 fn main() {
+    let link_flag = "pq";
+    let pkg_name = "libpq";
+
     if let Ok(lib_dir) = env::var("PQ_LIB_DIR") {
         println!("cargo:rustc-link-search=native={}", lib_dir);
+
+    } else if pkg_config::probe_library(pkg_name).is_ok() {
+        return // pkg_config does everything for us, including output for cargo
+
     } else if let Some(path) = pg_config_output() {
         let path = follow_dylib_symlinks(path.trim().into());
         println!("cargo:rustc-link-search=native={}", &path.display());
@@ -16,9 +25,8 @@ fn main() {
     } else {
         "dylib"
     };
-    let lib = "pq";
     
-    println!("cargo:rustc-link-lib={}={}", mode, lib);
+    println!("cargo:rustc-link-lib={}={}", mode, link_flag);
 }
 
 fn pg_config_output() -> Option<String> {

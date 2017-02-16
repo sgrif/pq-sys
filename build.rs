461ccf2b9dc0cc47a61cb355305569f3d0bf2712
Sean Griffin <sean@seantheprogrammer.com>
2017-02-16 09:57:26 -0500

    Update bindgen, remove libc dependency
    Recent versions of bindgen recommend generating the bindings from within
    a build script rather than including them statically. Additionally, we
    no longer have a libc dependency, since we only needed the basic type
    definitions and `std::os::raw` includes them.

--- build.rs
+++ build.rs
@@ -1,12 +1,16 @@
 #[cfg(feature="pkg-config")]
 extern crate pkg_config;
+extern crate bindgen;
 
+use self::bindgen::Builder;
 use std::fs;
 use std::path::PathBuf;
 use std::process::Command;
 use std::env;
 
 fn main() {
+    generate_bindgen_file();
+
     let link_flag = "pq";
 
     if let Ok(lib_dir) = env::var("PQ_LIB_DIR") {
@@ -15,7 +19,7 @@ fn main() {
     } else if configured_by_pkg_config() {
         return // pkg_config does everything for us, including output for cargo
 
-    } else if let Some(path) = pg_config_output() {
+    } else if let Some(path) = pg_config_output("--libdir") {
         let path = follow_dylib_symlinks(path.trim().into());
         println!("cargo:rustc-link-search=native={}", &path.display());
     }
@@ -39,9 +43,9 @@ fn configured_by_pkg_config() -> bool {
     false
 }
 
-fn pg_config_output() -> Option<String> {
+fn pg_config_output(command: &str) -> Option<String> {
     Command::new("pg_config")
-        .arg("--libdir")
+        .arg(command)
         .output()
         .ok()
         .into_iter()
@@ -61,4 +65,20 @@ fn follow_dylib_symlinks(libdir: PathBuf) -> PathBuf {
 #[cfg(not(target_os = "macos"))]
 fn follow_dylib_symlinks(libdir: PathBuf) -> PathBuf {
     libdir
+}
+
+fn generate_bindgen_file() {
+    let out_dir = env::var("OUT_DIR").unwrap();
+    let mut builder = Builder::default()
+        .no_unstable_rust()
+        .header("wrapper.h");
+
+    if let Some(path) = pg_config_output("--includedir") {
+        builder = builder.clang_arg(format!("-I{}", path));
+    }
+
+    builder.generate()
+        .expect("Unable to generate bindings for libpq")
+        .write_to_file(PathBuf::from(out_dir).join("bindings.rs"))
+        .expect("Unable to write bindings to file");
 }

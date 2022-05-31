c55ab302875b5639779d2e0caa9f1ae71ac2464b
Felix Fichte <dev@spruce.de>
2022-05-31 16:34:30 +0200

    Added accidentally removed line back

--- build.rs
+++ build.rs
@@ -141,6 +141,7 @@ fn configured_by_vcpkg() -> bool {
         println!("cargo:rustc-link-lib=gdi32");
         println!("cargo:rustc-link-lib=user32");
         println!("cargo:rustc-link-lib=secur32");
+        println!("cargo:rustc-link-lib=shell32");
     }).is_ok()
 }
 

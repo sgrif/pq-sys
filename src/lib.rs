fe54b081e52d51199a6adf9d729fbe9d12d2d82a
Georg Semmler <github@weiznich.de>
2023-12-15 10:35:47 +0100

    Add custom windows bindings

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@ extern crate pq_src;
 
 #[cfg(not(feature = "buildtime_bindgen"))]
 #[allow(non_camel_case_types, non_upper_case_globals, non_snake_case)]
+#[cfg_attr(target_os = "windows", path = "windows_bindings.rs")]
 mod bindings;
 
 #[cfg(feature = "buildtime_bindgen")]
